58932229b77db37ffc795c626faf63d035567fda
Nick Brawn <nickbrawn@Nicks-MBP.lan>
2020-07-12 12:50:22 +0100

    add select stories

---
+++ packages/nucleus-ui/src/select/index.stories.tsx
@@ -0,0 +1,55 @@
+import { action } from "@storybook/addon-actions";
+import React from "react";
+
+import { Select } from ".";
+
+export default {
+  component: Select,
+  title: "Select"
+};
+
+const selectOptions = [
+  { label: "Great Britain", value: "Great Britain" },
+  { label: "France", value: "France" },
+  { label: "Spain", value: "Spain" },
+  { label: "Germany", value: "Germany" }
+];
+
+const handleSelection = (value: any) => {
+  action("on selection", value)(value);
+};
+
+export const basicSelect = () => {
+  return (
+    <Select
+      hideSelectedOptions
+      onChange={handleSelection}
+      options={selectOptions}
+      placeholder="please select country"
+    />
+  );
+};
+
+export const MulitSelect = () => {
+  return (
+    <Select
+      hideSelectedOptions
+      isMulti
+      onChange={handleSelection}
+      options={selectOptions}
+      placeholder="please select country"
+    />
+  );
+};
+
+export const disabledSelect = () => {
+  return (
+    <Select
+      hideSelectedOptions
+      isDisabled
+      onChange={handleSelection}
+      options={selectOptions}
+      placeholder="please select country"
+    />
+  );
+};
---
+++ packages/nucleus-ui/src/select/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactSelect from "react-select";
+
+export const Select = (props: any) => {
+  return <ReactSelect {...props} />;
+};
