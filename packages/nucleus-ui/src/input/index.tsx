6e75070f2eb243cf3e9a5a28a1eb999c564deb76
James Leckenby <james.leckenby@sdh-ps.com>
2020-07-15 15:58:47 +0100

    Update input styling

--- packages/nucleus-ui/src/input/index.tsx
+++ packages/nucleus-ui/src/input/index.tsx
@@ -11,25 +11,25 @@ export function Input(props: InputProps): React.ReactElement {
     name,
     required,
     iconType: Icon,
-    positionIconLeft,
+    positionIconLeft
   } = props;
+  const iconPostionClass = positionIconLeft ? "left-0" : "right-0";
 
   // Build error classes
   let errorClassName = "";
   if (error) {
-    errorClassName = "pr-10 text-red-900 border-red-300 focus:border-red-300";
+    errorClassName = "pr-10 text-red-600 border-red-600 focus:border-red-600";
   }
 
   // Build props to pass to input
   const inputProperties = { ...props };
   delete inputProperties.componentRef;
+  delete inputProperties.error;
   delete inputProperties.required;
-
-  const iconPostionClass: string = positionIconLeft ? "left-0" : "right-0";
 
   return (
     <div className={className}>
-      <label className="block font-medium text-gray-700" htmlFor={name}>
+      <label className="block text-gray-600" htmlFor={name}>
         {label}
         {required && <span className="text-red-600"> *</span>}
       </label>
@@ -37,8 +37,8 @@ export function Input(props: InputProps): React.ReactElement {
         <input
           {...inputProperties}
           ref={componentRef}
-          className={`block w-full form-input ${errorClassName} ${
-            positionIconLeft ? " pl-8" : "pr-8"
+          className={`block w-full form-input rounded-none text-gray-900 focus:shadow-none focus:border-blue-500 ${errorClassName} ${
+            positionIconLeft ? " pl-10" : "pr-10"
           }`}
           id={name}
         />
@@ -46,14 +46,14 @@ export function Input(props: InputProps): React.ReactElement {
           <div
             className={`absolute inset-y-0 flex items-center pl-3 pr-3 pointer-events-none ${iconPostionClass}`}
           >
-            <Icon className="w-5 h-5" />
+            <Icon className="w-4 h-4" />
           </div>
         )}
       </div>
       {error && (
-        <div className="flex items-center p-1 ">
-          <FaExclamationCircle className="w-4 h-4 text-red-500" />
-          <p className="mt-1 mb-1 ml-1 text-xs text-red-600">{error.message}</p>
+        <div className="flex items-center p-1 text-red-600">
+          <FaExclamationCircle className="w-3 h-3" />
+          <p className="mt-1 mb-1 ml-1 text-xs">{error.message}</p>
         </div>
       )}
     </div>
