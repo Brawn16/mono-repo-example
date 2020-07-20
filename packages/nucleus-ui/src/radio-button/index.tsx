e3d31f7203cdf7be2ac80591322483c38f9dca1c
Nick Brawn <nickbrawn@Nicks-MacBook-Pro.local>
2020-07-20 15:04:39 +0100

    replace method with variable

--- packages/nucleus-ui/src/radio-button/index.tsx
+++ packages/nucleus-ui/src/radio-button/index.tsx
@@ -4,16 +4,13 @@ import { RadioInputTypes } from "./types";
 export const RadioButton = (props: RadioInputTypes) => {
   const { checked, onClick, label, value } = props;
 
-  const renderRadioButton = (isChecked: boolean | undefined) => {
-    if (isChecked) {
-      return (
-        <div className="absolute flex items-center justify-center w-4 h-4 mt-1 bg-blue-600 rounded-full">
-          <div className="w-1 h-1 bg-white rounded-full" />
-        </div>
-      );
-    }
-    return <div className="absolute w-4 h-4 mt-1 bg-gray-300 rounded-full" />;
-  };
+  const renderRadioButton = checked ? (
+    <div className="absolute flex items-center justify-center w-4 h-4 mt-1 bg-blue-600 rounded-full">
+      <div className="w-1 h-1 bg-white rounded-full" />
+    </div>
+  ) : (
+    <div className="absolute w-4 h-4 mt-1 bg-gray-300 rounded-full" />
+  );
 
   return (
     <label className="relative flex">
@@ -24,7 +21,7 @@ export const RadioButton = (props: RadioInputTypes) => {
         type="radio"
         value={value}
       />
-      {renderRadioButton(checked)}
+      {renderRadioButton}
       <span className=" ml-6">{label}</span>
     </label>
   );
