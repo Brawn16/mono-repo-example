5d41958b1b07383df84e5f461c5525af863cf14c
Nick Brawn <nickbrawn@Nicks-MacBook-Pro.local>
2020-07-20 11:23:43 +0100

    remove setTimeOut

--- packages/nucleus-ui/src/spinner/index.tsx
+++ packages/nucleus-ui/src/spinner/index.tsx
@@ -1,18 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ClipLoader } from "react-spinners";
 import { LoaderSizeProps } from "react-spinners/interfaces";
 
 export function Spinner(props: LoaderSizeProps) {
-  const [spin, setSpin] = useState(false);
   const { color = "gray" } = props;
-
-  useEffect(() => {
-    setTimeout(() => setSpin(true), 200);
-  }, []);
-
-  if (!spin) {
-    return null;
-  }
-
   return <ClipLoader {...props} color={color} />;
 }
