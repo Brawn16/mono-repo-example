a97da55f0e069714fa989ee66638b64bbd5749bd
Nick Brawn <nickbrawn@Nicks-MacBook-Pro.local>
2020-08-05 09:56:33 +0100

    change display none on mobile so it sits within the steps component

--- packages/nucleus-frontend/layouts/new-starter/index.tsx
+++ packages/nucleus-frontend/layouts/new-starter/index.tsx
@@ -6,40 +6,40 @@ import { DataProvider } from "./context";
 const steps = [
   {
     href: "/new-starter",
-    label: "Preparation",
+    label: "Preparation"
   },
   {
     href: "/new-starter/personal-details",
-    label: "Personal Details",
+    label: "Personal Details"
   },
   {
     href: "/new-starter/address",
-    label: "Address",
+    label: "Address"
   },
   {
     href: "/new-starter/work-details",
-    label: "Work Details",
+    label: "Work Details"
   },
   {
     href: "/new-starter/qualifications",
-    label: "Qualifications",
+    label: "Qualifications"
   },
   {
     href: "/new-starter/identification",
-    label: "Identification",
+    label: "Identification"
   },
   {
     href: "/new-starter/medical",
-    label: "Medical",
+    label: "Medical"
   },
   {
     href: "/new-starter/summary",
-    label: "Summary",
-  },
+    label: "Summary"
+  }
 ];
 
 export function NewStarter({
-  children,
+  children
 }: React.PropsWithChildren<{}>): React.ReactElement {
   const { route } = useRouter();
   const year = new Date().getFullYear();
@@ -59,9 +59,7 @@ export function NewStarter({
           <h1 className="py-4 text-3xl font-light text-center uppercase">
             New Starter Form
           </h1>
-          <span className="hidden md:block">
-            <Steps active={active} steps={steps} />
-          </span>
+          <Steps active={active} steps={steps} />
           <span>
             <div className="flex items-center justify-center  w-full h-10 text-white bg-orange-500 text-bold md:hidden">
               {active?.toUpperCase()}
