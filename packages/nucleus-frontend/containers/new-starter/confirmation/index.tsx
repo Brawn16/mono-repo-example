fb4db8b4d20834700db9fd50e96b012af25fa842
James Leckenby <james.leckenby@sdh-ps.com>
2020-09-14 16:41:22 +0100

    Update confirmation text

--- packages/nucleus-frontend/containers/new-starter/confirmation/index.tsx
+++ packages/nucleus-frontend/containers/new-starter/confirmation/index.tsx
@@ -9,7 +9,7 @@ export function Confirmation() {
       <NewStarterLayout showSteps={false} title="New Starter Confirmation">
         Congratulations, you have now completed the online onboarding. We are
         dealing with your application and will notify you of the outcome via
-        contact details provided within X working days.
+        contact details provided within 7 working days.
         <div className="mt-4">
           If for any reason, we are unable to meet the deadline or require
           further information, we will contact you.
